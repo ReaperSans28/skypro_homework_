8dc863ac71b096bdeacc896db1a54833217758f2
Bogdanchik <bogdanleontev59@gmail.com>
2024-05-14 17:57:41 +0300

    added utils.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,36 +1,27 @@
-from typing import Any, Callable, Dict, Tuple, Optional
+from typing import Any, Callable, Dict, Optional, Tuple
 
 
 def log(file_name: Optional[str] = None) -> Callable[[Callable], Callable]:
-    """
-    Функция принимающая имя файла, если имени нет, то все выведется в консоль.
-    """
-
     def decorator(func: Callable) -> Callable:
-        """
-        Функция принимающая функцию my_function.
-        """
 
         def decorator_(*args: Tuple[Any], **kwargs: Dict[str, Any]) -> Any:
-            """
-            Функция проверяющая результат работы функции my_function и записывающая его в указанный файл
-            Если файла нет, то вывод происходит в консоль.
-            """
             try:
                 result = func(*args, **kwargs)
             except Exception as error:
-                try:
-                    with open(file_name, "a") as file:
-                        file.write(f"2018-10-31 20:31:00 {func.__name__} error: {error}. Inputs: {args}, {kwargs}\n")
-                except FileNotFoundError:
-                    print(f"2018-10-31 20:31:00 {func.__name__} error: {error}. Inputs: {args}, {kwargs}\n")
+                log_message = f"2018-10-31 20:31:00 {func.__name__} error: {error}. Inputs: {args}, {kwargs}\n"
             else:
+                log_message = f"2018-10-31 20:31:00 {func.__name__} ok\n"
+
+            if file_name is not None:
                 try:
                     with open(file_name, "a") as file:
-                        file.write(f"2018-10-31 20:31:00 {func.__name__} ok\n")
+                        file.write(log_message)
                 except FileNotFoundError:
-                    print(f"2018-10-31 20:31:00 {func.__name__} ok")
-                return result
+                    print(log_message)
+            else:
+                print(log_message)
+
+            return result
 
         return decorator_
 
@@ -39,7 +30,4 @@ def log(file_name: Optional[str] = None) -> Callable[[Callable], Callable]:
 
 @log(file_name="")
 def my_function(x: int, y: int) -> int:
-    """
-    Функция сложения двух чисел.
-    """
     return x + y
---
+++ src/utils.py
@@ -0,0 +1,86 @@
+import json
+from typing import Dict, List, Optional
+
+import requests
+
+
+def get_financial_transactions(request: str) -> List[Dict]:
+    """
+    Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
+    Иначе пустой список.
+    """
+    try:
+        with open(request, encoding="utf-8") as f:
+            data = json.load(f)
+        if not isinstance(data, list):
+            return []
+        transactions = []
+        for item in data:
+            if "operationAmount" in item and isinstance(item["operationAmount"], dict):
+                if "amount" in item["operationAmount"]:
+                    transactions.append(item)
+        return transactions
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        return []
+
+
+def get_usd_value() -> Optional[float]:
+    """Функция выдает актуальный курс доллара, если он доступен."""
+    url = "https://www.cbr-xml-daily.ru/daily_json.js"
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        usd_rate_str = data["Valute"]["USD"]["Value"]
+        usd_rate = float(usd_rate_str.replace(",", "."))  # Преобразование строки в float
+        return usd_rate
+    else:
+        return None
+
+
+def get_euro_value() -> Optional[float]:
+    """Функция выдает актуальный курс евро, если он доступен."""
+    url = "https://www.cbr-xml-daily.ru/daily_json.js"
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        usd_rate_str = data["Valute"]["EUR"]["Value"]
+        usd_rate = float(usd_rate_str.replace(",", "."))
+        return usd_rate
+    else:
+        return None
+
+
+def get_amount_transactions(request: str) -> List[float]:
+    """
+    Принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях, возвращает тип float.
+    Если транзакция была в USD или EUR, идет обращение к функциям, которые дадут актуальный курс валюты и
+    происходит конвертация сумм.
+    """
+    try:
+        with open(request, encoding="utf-8") as f:
+            data = json.load(f)
+        if not isinstance(data, list):
+            return [0.0]
+
+        amount = []
+        for item in data:
+            if "operationAmount" in item:
+                amount.append(item["operationAmount"])
+
+        amount_rub = []
+        for value in amount:
+            if value["currency"] == {"name": "USD", "code": "USD"}:
+                usd_value = get_usd_value()
+                if usd_value is not None:
+                    amount_rub.append(float(value["amount"]) * usd_value)
+            elif value["currency"] == {"name": "EUR", "code": "EUR"}:
+                euro_value = get_euro_value()
+                if euro_value is not None:
+                    amount_rub.append(float(value["amount"]) * euro_value)
+            else:
+                amount_rub.append(float(value["amount"]))
+
+        return amount_rub
+
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        return [0.0]
