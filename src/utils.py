68f22026710445f95fcc43caf24866b27b7ed85d
Bogdanchik <bogdanleontev59@gmail.com>
2024-05-20 17:03:08 +0300

    changed utils and test_utils

--- src/utils.py
+++ src/utils.py
@@ -30,59 +30,54 @@ def get_financial_transactions(request: str) -> List[Dict]:
         return []
 
 
-def get_usd_value(api_key: str) -> Optional[float]:
+def get_usd_value() -> Optional[float]:
+    """Выдает актуальный курс доллара"""
+    global api
     url = "https://open.er-api.com/v6/latest/USD"
-    headers = {"apikey": api_key}
+    headers = {"apikey": api}
     response = requests.get(url, headers=headers)
     data = response.json()
     if response.status_code == 200 and "conversion_rates" in data:
-        if "RUB" in data["conversion_rates"]:
-            usd_rate = data["conversion_rates"]["RUB"]
+        if "USD" in data["conversion_rates"]:
+            usd_rate = data["conversion_rates"]["USD"]
             return float(usd_rate)
     return None
 
 
-def get_euro_value(api_key: str) -> Optional[float]:
+def get_euro_value() -> Optional[float]:
+    """Выдает актуальный курс евро"""
+    global api
     url = "https://open.er-api.com/v6/latest/EUR"
-    headers = {"apikey": api_key}
+    headers = {"apikey": api}
     response = requests.get(url, headers=headers)
     data = response.json()
     if response.status_code == 200 and "conversion_rates" in data:
-        if "RUB" in data["conversion_rates"]:
-            euro_rate = data["conversion_rates"]["RUB"]
+        if "EUR" in data["conversion_rates"]:
+            euro_rate = data["conversion_rates"]["EUR"]
             return float(euro_rate)
     return None
 
 
-def get_amount_transactions(request: str, api_key: str) -> float:
-    try:
-        with open(request, encoding="utf-8") as f:
-            data = json.load(f)
-        if not isinstance(data, list):
-            return 0.0
-
-        amount_rub = 0.0
-        for item in data:
-            if "operationAmount" in item:
-                value = item["operationAmount"]
-                currency = value.get("currency", {}).get("code", "")
-                if currency == "USD":
-                    usd_value = get_usd_value(api_key)
-                    if usd_value is not None:
-                        amount_rub += float(value["amount"]) * usd_value
-                elif currency == "EUR":
-                    euro_value = get_euro_value(api_key)
-                    if euro_value is not None:
-                        amount_rub += float(value["amount"]) * euro_value
-                else:
-                    amount_rub += float(value["amount"])
-
-        return amount_rub
-
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
-        return 0.0
-
-
-path = "C:\\Users\\student\\PycharmProjects\\skypro_homework1828\\data\\data.json"
-converted_amount = get_amount_transactions(path, api)
-print(f"Converted amount in rubles: {converted_amount}")
+def get_amount_transactions(data: list) -> float:
+    """
+    Функция принимает на вход список словарей с данными о финансовых транзакциях и возвращает сумму всех транзакций
+    в рублях.
+    Иначе 0.0
+    """
+    global api
+    amount_rub = 0.0
+    for item in data:
+        if "operationAmount" in item:
+            value = item["operationAmount"]
+            currency = value.get("currency", {}).get("code", "")
+            if currency == "USD":
+                usd_value = get_usd_value()
+                if usd_value is not None:
+                    amount_rub += float(value["amount"]) * usd_value
+            elif currency == "EUR":
+                euro_value = get_euro_value()
+                if euro_value is not None:
+                    amount_rub += float(value["amount"]) * euro_value
+            else:
+                amount_rub += float(value["amount"])
+    return amount_rub
